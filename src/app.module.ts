f4620241d31a5a9cb2a1dce4a799bea51447f3b6
pratikash <pratikash@ncompass.inc>
2024-02-15 12:26:24 +0530

    modified app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,19 +13,20 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true
+      isGlobal: true,
     }),
     TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    entities: [User, Topic, Blog, Editor, Viewer],
-    synchronize: true
-  }),
-    UsersModule],
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User, Topic, Blog, Editor, Viewer],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
